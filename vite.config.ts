61ef8fd2912ffe48f6f8ce5e2341d07103318d83
SKAT1005 <111747034+SKAT1005@users.noreply.github.com>
2023-09-18 12:55:36 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,50 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import NodeModulesPolyfillPlugin from '@esbuild-plugins/node-modules-polyfill';
 import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
-import {nodeResolve} from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            util: 'web-encoding'
-        },
+ server: {
+    https: true,
+    host: 'socialland.ru',
+    port: 443
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      util: 'web-encoding',
     },
-    optimizeDeps: {
-        esbuildOptions: {
-            // Node.js global to browser globalThis
-            define: {
-                global: 'globalThis',
-            },
-            // Enable esbuild polyfill plugins
-            plugins: [
-                NodeGlobalsPolyfillPlugin({
-                    buffer: true
-                }),
-            ],
-        },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Преобразование глобальных переменных Node.js в globalThis для браузера
+      define: {
+        global: 'globalThis',
+      },
+      // Включение esbuild плагина для полифиллинга Node.js
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
     },
-    build: {
-        target: ['ESNext'],
-        rollupOptions: {
-            plugins: [
-                nodePolyfills(),
-                NodeModulesPolyfillPlugin(),
-                nodeResolve({browser: true}),
-            ]
-        },
-        commonjsOptions: {
-            transformMixedEsModules: true // fuck this
-        }
-    }
+  },
+  build: {
+    // Указание цели сборки на ESNext
+    target: ['ESNext'],
+    rollupOptions: {
+      plugins: [
+        // Использование плагина для полифиллинга Node.js
+        nodePolyfills(),
+        NodeModulesPolyfillPlugin(),
+        nodeResolve({ browser: true }),
+      ],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true, // Включение опции для смешанных модулей ES
+    },
+  },
 });
